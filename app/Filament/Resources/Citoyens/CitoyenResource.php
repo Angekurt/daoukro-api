c7da407c5fc8b00ce5257856ce4406d84b4f1848
Angekurt <Angekurt@users.noreply.github.com>
2026-08-10 05:36:38 +0000

    Supprime navigationGroup de CitoyenResource (conflit type Filament)

--- app/Filament/Resources/Citoyens/CitoyenResource.php
+++ app/Filament/Resources/Citoyens/CitoyenResource.php
@@ -27,7 +27,6 @@ class CitoyenResource extends Resource
     protected static ?string $model = Citoyen::class;
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
     protected static ?string $navigationLabel = 'Comptes Pros';
-    protected static string|BackedEnum|null $navigationGroup = null;
     protected static ?string $recordTitleAttribute = 'name';
 
     public static function getNavigationBadge(): ?string
